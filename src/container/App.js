968d87140d1d1718546a7cf1be8e9b0eed740f7a
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-19 17:52:02 -0500

    modified

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,30 @@
-
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "../components/Header/Header.jsx";
-import Filter from "../components/Search/Filter.jsx"
+import Filter from "../components/Filter/Filter.jsx";
+import Table from "../components/Table/Table.jsx";
+import API from "../utils/API";
 
 function App() {
+  // state constants to store results from api-call
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    // call API and set the result using set result
+    API.fetchEmployees()
+      .then((res) => {
+        setResult(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Header/>
-      <Filter/>
+      <Header />
+      <Filter />
+      <Table listOfEmployees={result} />
     </div>
   );
 }
