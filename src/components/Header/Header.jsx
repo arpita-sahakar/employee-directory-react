2a0399ab4ce4efe1a79c09a1de95d9697cf5ae03
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-19 16:16:45 -0500

    added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <article className="mw10 center ph3 ph5-ns tc br2 pv5 bg-washed-red dark-green mb5">
+      <h1 className="fw6 f3 f2-ns lh-title mt0 mb3">Employee Directory</h1>
+    </article>
+  );
+};
+
+export default Header;
--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,11 @@
 
 import "./App.css";
+import Header from "../components/Header/Header.jsx"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header/>
     </div>
   );
 }
